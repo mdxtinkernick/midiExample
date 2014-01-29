f5dfcdf70e4f67718f9348a495d079a77ea3cf36
nick weldin <n.weldin@mdx.ac.uk>
2014-01-29 13:36:08 +0000

    added offset to give betterpitch range

--- midiExample.ino
+++ midiExample.ino
@@ -12,17 +12,19 @@ void loop(){
   //comment out this as it will be sent along with the midi data and mess up things
   //Serial.println(reading);
   
-  //Send midi note using better value for pitch
-  midiNote(0x90, betterValue, 127);
+  //betterValue by itself was too low (pitch 0 is very low
+  //we need to add an offset to it
+  int startNote=50;
+  midiNote(0x90, startNote+betterValue, 127);
   delay(10);
-  midiNote(0x90, betterValue, 0);
+  midiNote(0x90, startNote+betterValue, 0);
   delay(10);
 
   
 }
 
 
-//this function is never called
+//this function is now called twice each time thorough the loop 
 void midiNote(int cmd, int pitch, int vel){ 
   Serial.write(cmd);
   Serial.write(pitch);
