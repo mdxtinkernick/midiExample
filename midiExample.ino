a16eed778d04796489fc47cf37b01383195bd2ed
nick weldin <n.weldin@mdx.ac.uk>
2014-02-05 09:45:57 +0000

    added ther abilty to play a scale, using an array

--- midiExample.ino
+++ midiExample.ino
@@ -1,3 +1,7 @@
+int scale[]={0, 2, 4, 5, 7, 9, 11, 12};
+//int scale[]={0, 2, 3, 5, 7, 9, 11, 12};
+
+
 void setup(){
   //change serial speed to correct speed for midi
   Serial.begin(31250); 
@@ -8,16 +12,19 @@ void loop(){
   int reading = analogRead(A0);
   //50 to 500 is the range of values I got with my sensor
   //0 to 11 will cover an octave in semitones
-  int betterValue=map(reading, 50, 500, 0, 11);
+  int betterValue=map(reading, 50, 500, 0, 8);
   //comment out this as it will be sent along with the midi data and mess up things
   //Serial.println(reading);
+  int startNote=50;
+  //get it to play in a musical scale
+  int noteToPlay= startNote + scale[betterValue];
   
   //betterValue by itself was too low (pitch 0 is very low
   //we need to add an offset to it
-  int startNote=50;
-  midiNote(0x90, startNote+betterValue, 127);
+  
+  midiNote(0x90, noteToPlay, 127);
   delay(200);
-  midiNote(0x90, startNote+betterValue, 0);
+  midiNote(0x90, noteToPlay, 0);
   delay(10);
 
   
